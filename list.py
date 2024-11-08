991f409513028e83b263e75acdf2b87d72cc62c7
ATOROYO <davidatoroyosika@gmail.com>
2024-11-08 17:42:16 +0300

    Accessed item index 0

--- list.py
+++ list.py
@@ -9,3 +9,7 @@ print(my_list)
 # List with mixed data types
 my_list2 = [1, "hello", 3.4]
 print(my_list2)
+
+# Access Python List Elements
+languages = ["Python", "Swift", "C++"]
+print(languages[0])
