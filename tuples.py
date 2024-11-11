dac84b43206aa95ca97fe6e075e10e9512294f4b
ATOROYO <davidatoroyosika@gmail.com>
2024-11-11 19:21:32 +0300

    Wrote for optional parentheis

--- tuples.py
+++ tuples.py
@@ -5,3 +5,7 @@ print(type(var1)) # <class 'str'>
 # Creating a tuple having one element
 var2 = ("hello")
 print(type(var2)) # <class 'tuple'>
+
+# Parenthesis is optional
+var3 = "hello"
+print(type(var3)) # <class 'tuple'>
