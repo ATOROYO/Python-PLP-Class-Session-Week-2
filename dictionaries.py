7b1bb2fb2925d72240fb728edc0add1df8f35104
ATOROYO <davidatoroyosika@gmail.com>
2024-11-11 19:37:51 +0300

    Printed the numbers

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,7 @@
 # Create a dictionary in Python
 capital_city = {"Nepal": "Kathmandu", "Italy": "Rome"}
 print(capital_city)
+
+# Dictionaries with keys and values
+numbers = {1: "One", 2: "Two", 3: "Three"}
+print(numbers)
