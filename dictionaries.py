7a995a62fbce2060e13368e101e3571d8c338127
ATOROYO <davidatoroyosika@gmail.com>
2024-11-11 19:40:01 +0300

    Added initial code for dic

--- dictionaries.py
+++ dictionaries.py
@@ -1,7 +1,10 @@
 # Create a dictionary in Python
-capital_city = {"Nepal": "Kathmandu", "Italy": "Rome"}
+capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
 print(capital_city)
 
 # Dictionaries with keys and values
 numbers = {1: "One", 2: "Two", 3: "Three"}
 print(numbers)
+
+# Add Elements to python dictionaries
+print("Initial Dictionary: ", capital_city)
