7f01f022ca4429b0c2950ecd78b51625a42376bc
ATOROYO <davidatoroyosika@gmail.com>
2024-11-11 19:19:44 +0300

    Created a tuple with one element

--- tuples.py
+++ tuples.py
@@ -1,3 +1,7 @@
 # Creating a Python Tuple With one Element
 var1 = ("hello")
 print(type(var1)) # <class 'str'>
+
+# Creating a tuple having one element
+var2 = ("hello")
+print(type(var2)) # <class 'tuple'>
