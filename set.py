f45717a76ef3956f85d3547ef514332aa5b5710c
ATOROYO <davidatoroyosika@gmail.com>
2024-11-12 14:14:32 +0300

    Created a set of String type

--- set.py
+++ set.py
@@ -1,3 +1,7 @@
 # Create a set of interger type
 student_id = {112, 114, 116, 118, 115}
 print("Student ID: ", student_id)
+
+# Create a set of string type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print("Vowels Letters: ", vowel_letters)
