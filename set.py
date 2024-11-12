c6aa6357d1a6626ab04420c852409887456143a3
ATOROYO <davidatoroyosika@gmail.com>
2024-11-12 15:24:25 +0300

    Find Number of Set Elements

--- set.py
+++ set.py
@@ -43,3 +43,12 @@ fruits = {"Apple", "Peach", "Mango"}
 # for Loop to access each fruits
 for fruit in fruits:
     print(fruit)
+    
+# Find Number of Set Elements
+even_numbers = {2,4,6,8}
+print("Set:", even_numbers)
+# find number of elements
+print("Total Elements: ", len(even_numbers))
+# Output
+# Set: {8,2,4,6}
+# Total Elements: 4
