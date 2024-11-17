65d43f2adc78cf4b9f4c88e2be942abb70b20932
ATOROYO <davidatoroyosika@gmail.com>
2024-11-17 20:24:37 +0300

    Did challenge 2

--- challenge.py
+++ challenge.py
@@ -1,3 +1,4 @@
+# QUESTION 1
 # Accept user input to create a list of integers
 numbers = input("Enter a list of integers, separated by spaces: ")
 
@@ -9,3 +10,12 @@ total_sum = sum(numbers_list)
 
 # Output the result
 print("The sum of the integers in the list is:", total_sum)
+
+# QUESTION 2
+# Create a tuple with five favorite book names
+favorite_books = ("The Great Gatsby", "1984", "To Kill a Mockingbird", "Pride and Prejudice", "Moby-Dick")
+
+# Use a for loop to print each book name on a separate line
+for book in favorite_books:
+    print(book)
+
